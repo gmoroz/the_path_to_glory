7e5de6095f83b1dc51db822097aa64ecca69e442
rorymercury <12345hasek@gmail.com>
2022-12-02 20:46:08 +0300

    add playerunit and enemyunit

--- skills.py
+++ skills.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
@@ -11,6 +12,8 @@ class Skill(ABC):
     name: str
     stamina: float
     damage: float
+    user: None | BaseUnit = None
+    target: None | BaseUnit = None
 
     @abstractmethod
     def skill_effect(self):
@@ -19,17 +22,17 @@ class Skill(ABC):
     def _is_stamina_enough(self) -> bool:
         return self.user.stamina >= self.stamina
 
-    def use(self, user: BaseUnit, target: BaseUnit):
+    def use(self, user: BaseUnit, target: BaseUnit) -> str:
         self.user = user
         self.target = target
-        if self._is_stamina_enough:
+        if self._is_stamina_enough():
             return self.skill_effect()
         return f"{self.user.name} попытался использовать {self.name} но у него не хватило выносливости."
 
 
 @dataclass
 class ConcreteSkill(Skill):
-    def skill_effect(self):
+    def skill_effect(self) -> str:
         self.user.stamina -= self.stamina
-        self.target.get_damage(self.user.)
+        self.target.get_damage(self.damage)
         return f"{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику."
--- unit.py
+++ unit.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from random import choice
 from classes import UnitClass
 from equipment import Armor, Weapon
 
@@ -12,7 +13,7 @@ class BaseUnit(ABC):
         self.stamina = unit_class.max_stamina
         self.weapon = ...
         self.armor = ...
-        self._is_skill_used = ...
+        self._is_skill_used = False
 
     @property
     def health_points(self) -> str:
@@ -30,10 +31,12 @@ class BaseUnit(ABC):
         self.armor = armor
         return f"{self.name} экипирован броней {self.armor.name}"
 
-    def _count_damage(self, target: BaseUnit):
+    def _count_damage(self, target: BaseUnit) -> int:
         damage = self.weapon.calc_damage()
         if target.stamina >= target.armor.stamina_per_turn:
             damage -= target.armor.defence * target.unit_class.armor
+            target.stamina -= target.armor.stamina_per_turn
+        return damage
 
     def get_damage(self, damage: float) -> None:
         self.hp -= damage
@@ -43,4 +46,29 @@ class BaseUnit(ABC):
         pass
 
     def use_skill(self, target: BaseUnit) -> str:
-        pass
+        if self._is_skill_used:
+            return "Навык уже использован."
+        return self.unit_class.skill.use(user=self, target=target)
+
+
+class PlayerUnit(BaseUnit):
+    def hit(self, target: BaseUnit) -> str:
+        if self.unit_class.stamina >= self.weapon.stamina_per_hit:
+            damage = self._count_damage(target)
+            if damage > 0:
+                return f"{self.name} используя {self.weapon.name} пробивает {target.armor.name} соперника и наносит {damage} урона."
+            return f"{self.name} используя {self.weapon.name} наносит удар, но {target.armor.name} cоперника его останавливает."
+        return f"{self.name} попытался использовать {self.weapon.name}, но у него не хватило выносливости."
+
+
+class EnemyUnit(BaseUnit):
+    def hit(self, target: BaseUnit) -> str:
+        if not self._is_skill_used and choice(range(5)) == 4:
+            self._is_skill_used = True
+            return self.use_skill(target)
+        if self.unit_class.stamina >= self.weapon.stamina_per_hit:
+            damage = self._count_damage(target)
+            if damage > 0:
+                f"{self.name} используя {self.weapon.name} пробивает {target.armor.name} и наносит Вам {damage} урона."
+            f"{self.name} используя {self.weapon.name} наносит удар, но Ваш(а) {target.armor.name} его останавливает."
+        f"{self.name} попытался использовать {self.weapon.name}, но у него не хватило выносливости."
