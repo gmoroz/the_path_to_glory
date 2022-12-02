359682001e2aaf1a9975afdb4f9574666ebb112a
rorymercury <12345hasek@gmail.com>
2022-12-02 23:17:32 +0300

    update tests

--- test_classes.py
+++ test_classes.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from logic.classes import get_unit_classes
 
-#
-# class TestUnitClass:
-# unit_classes = get_unit_classes()
-#
-print(get_unit_classes())
+if TYPE_CHECKING:
+    from logic.classes import UnitClass
+
+
+unit_classes = get_unit_classes()
+warrior: UnitClass = unit_classes["Воин"]
+mage: UnitClass = unit_classes["Маг"]
+thief: UnitClass = unit_classes["Вор"]
+assert warrior.skill.name == "Свирепый пинок"
