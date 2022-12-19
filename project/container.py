44ae8ba7d39fe41278914188a9136232738db98d
R0ryMercury <12345hasek@gmail.com>
2022-12-19 07:47:13 +0300

    delete user schema

--- project/container.py
+++ project/container.py
@@ -1,7 +1,6 @@
 from project.logic.equipment import Equipment
 from project.logic.classes import get_unit_classes
 from project.service.user import UserService
-from project.dao.models.user import UserSchema
 from project.dao.user import UserDao
 from project.setup_db import db
 
@@ -10,4 +9,3 @@ unit_classes = get_unit_classes()
 
 user_dao = UserDao(session=db.session)
 user_service = UserService(user_dao)
-user_schema = UserSchema()
--- project/dao/models/user.py
+++ project/dao/models/user.py
@@ -11,14 +11,3 @@ class User(db.Model):
     last_name = db.Column(db.String(80), nullable=False)
     wins_count = db.Column(db.Integer(), default=0)
     loses_count = db.Column(db.Integer(), default=0)
-
-
-class UserSchema(Schema):
-    id = fields.Int()
-    username = fields.Str()
-    email = fields.Email()
-    password = fields.Str()
-    first_name = fields.Str()
-    last_name = fields.Str()
-    wins_count = fields.Int()
-    loses_count = fields.Int()
